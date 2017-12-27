6b7dc301ce6b6f7cd9098bb43947d043a2c2c874
qiwang <timer_sevenwang@163.com>
2017-12-27 10:45:10 +0800

    update

---
+++ SWCommonFramework/Pods/Target Support Files/Pods-SWCommonFramework/Pods-SWCommonFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SWCommonFramework : NSObject
+@end
+@implementation PodsDummy_Pods_SWCommonFramework
+@end
---
+++ SWCommonFramework/SWCommonFramework/SWCommonFramework.h
@@ -0,0 +1,19 @@
+//
+//  SWCommonFramework.h
+//  SWCommonFramework
+//
+//  Created by seven on 2017/12/27.
+//  Copyright © 2017年 seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWCommonFramework.
+FOUNDATION_EXPORT double SWCommonFrameworkVersionNumber;
+
+//! Project version string for SWCommonFramework.
+FOUNDATION_EXPORT const unsigned char SWCommonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWCommonFramework/PublicHeader.h>
+
+
